32ed55820377cc37f224d3b0546ad7df21b9b558
hackerswastik <swastiksrivastava11@gmail.com>
2025-09-23 20:43:35 +0530

    update: replace hero banner with banner-1.png

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -41,7 +41,7 @@ export default function Hero() {
       <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: `url(/assets/profile.jpg)`,
+          backgroundImage: `url(/assets/banner-1.png)`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           filter: 'brightness(0.6)',
